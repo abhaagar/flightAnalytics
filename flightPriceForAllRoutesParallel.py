661240d546b5d0e8865ecfaf0dbeeb4766d737d0
Abhinav <abhinav@www.squirrelmail.com>
2016-01-02 19:19:55 +0530

    first commit

---
+++ flightPriceForAllRoutesParallel.py
@@ -0,0 +1,338 @@
+#!/usr/bin/python
+import sys, re, gc, time, signal, os, pdb
+import json
+import MySQLdb
+import datetime,time
+import multiprocessing, Queue
+import flightUtil
+import traceback
+
+
+#isTracingEnabled = os.environ['TRACE']
+isTracingEnabled = True
+flightPrefix = ''
+flightSuffix = ''
+unchangedFlightsPrefix = 'Unchanged Flights :'
+changedFlightsPrefix = 'Changed Flights :'
+newFlightsPrefix = 'New Flights :'
+
+unchangedFlights = ''
+changedFlights = ''
+newFlights = ''
+
+retryQueue = Queue.Queue() 
+
+def cityQuery():
+   return "SELECT code FROM cityWithCode WHERE isAvailable=1 order by code"
+
+def cityPairList():
+   cnx = flightUtil.connection()
+   cur = cnx.cursor()
+   cur.execute(cityQuery())
+   dataDuplicate = cur.fetchall()
+   data = set(dataDuplicate)
+   cnx.close()
+   return [(city1[0],city2[0]) for city1 in data for city2 in data if city1[0]!=city2[0]]
+def createSelectQuery(flight):
+   selectQuery = " SELECT price "\
+                 " FROM flight "\
+                 " WHERE name='%s'" \
+                 " ORDER BY id DESC "\
+                 " LIMIT 1"%flight
+   return selectQuery
+
+def createInsertQuery(flight,price):
+   insertQuery = " INSERT INTO flight "\
+                 " (name,price) "\
+                 " VALUES (%s,%s)"
+   return insertQuery
+
+def insertFlightUpdatedPrice(cur,flight,price):
+   insertQuery = createInsertQuery(flight,price)
+   cur.execute(insertQuery,(flight,price))
+   if cur.rowcount!=1: 
+      print 'Insertion Failed'
+      sys.exit(1)
+   cur.execute('COMMIT')
+
+def checkAndInsertFlightPrice(flight,price):
+   global cnx, changedFlights, unchangedFlights, newFlights 
+   cur = cnx.cursor()
+   selectQuery = createSelectQuery(flight)
+   cur.execute(selectQuery)	
+   data = cur.fetchone()
+   if cur.rowcount==0:
+      insertFlightUpdatedPrice(cur,flight,price)
+      newFlights += ' '+flight
+   elif data[0]!=int(price):
+      insertFlightUpdatedPrice(cur,flight,price)
+      changedFlights += ' '+flight
+   else:
+      unchangedFlights += ' '+flight
+
+def printFlightsInScedule(key,value):
+   global flightPrefix
+   global flightSuffix
+   for key1,value1 in value.items():
+      if re.match(flightPrefix,key1) is not None and key1.endswith(flightSuffix): 
+         checkAndInsertFlightPrice(key1,value1['O']['ADT']['tf'])
+
+def logError(traceBack,information,typ):
+   return #$
+   c = flightUtil.connection()
+   cur = c.cursor()
+   query = flightUtil.errorLoggingQuery%(traceBack,information,typ)
+   print query
+   cur.execute(query)
+   cur.fetchall()
+   cur.execute(flightUtil.commit)
+     
+
+def resetTheAvailability():
+   c = flightUtil.connection()
+   cur = c.cursor()
+   cur.execute(flightUtil.resetFlightAvailabilityQuery)
+   cur.fetchall()
+   cur.execute(flightUtil.commit)
+   cur.fetchall()
+   cur.execute(flightUtil.resetDirectFlightsDetailsAvailabilityQuery)
+   cur.fetchall()
+   cur.execute(flightUtil.commit)
+   cur.fetchall()
+   cur.execute(flightUtil.resetOneStopFlightsDetailsAvailabilityQuery)
+   cur.fetchall()
+   cur.execute(flightUtil.commit)
+   cur.fetchall()
+   cur.execute(flightUtil.resetTwoStopFlightsDetailsAvailabilityQuery)
+   cur.fetchall()
+   cur.execute(flightUtil.commit)
+   cur.fetchall()
+
+
+def fetchParsedJson(origin,destination,date):
+   successOuter = False
+   retry = 3
+   parsed_json = ''
+
+   error,string = flightUtil.fetchSanitizedInput(origin,destination,date)
+   if not error:
+      try :
+         parsed_json = json.loads(string)
+      except Exception,e:
+         print 'Error :: Invalid Json '+str(e)
+         logError(str(e),str(date+' '+origin+' '+destination),1)
+   return parsed_json
+
+
+
+def parseFlightScheduleAndStoreDetails(flightSchedules,pattern,route,date):
+   for i in range(len(flightSchedules)):
+      try:
+         flights = ''
+         flightPrices = ''
+         try:
+            flights = flightSchedules[i]['fltSchedule'][pattern]
+            if isTracingEnabled:
+               print 'Flight Schedule %d Parsed'%i
+            flightPrices = flightSchedules[i]['fareDetails'][pattern]
+            if isTracingEnabled:
+               print 'Fare Details of Flight Schedule %d Parsed'%i
+         except Exception,e:
+            if flights=='':
+               print 'Error :: Invalid or Absent fltSchedules '+str(e)
+               print flightSchedule[i]
+               continue
+         iterate = len(flights)
+         if flightPrices=='':
+            print 'Flight Schedules without fareDetails'
+         if isTracingEnabled:
+            print 'Total Flights: %s'%str(iterate)
+         for j in range(iterate):
+            isStoredSuccessfuly = False
+            try:
+               flight = flights[j]
+               flightId = flight['ID']
+               if isTracingEnabled:
+                  print 'Flight id %s'%flightId
+               if len(flight['OD'])!=1:
+                  print 'Multiple OD entries'
+                  continue
+               for fl in flight['OD']:
+                  timings = ''
+                  price = -1
+                  if flightPrices!='':
+                     price = int(flightPrices[flightId]['O']['ADT']['tf'])
+                     if isTracingEnabled:
+                        print 'Fare of Flight parsed'
+                  for t in fl['FS']:
+                     timings += t['dd']+' '+t['ad']+' '
+                  args = \
+                     [flightId,price,timings,route.getSource(),route.getDestination()]
+                  if isTracingEnabled:
+                     print 'Flight Timimgs Parsed'
+                     print 'Executing Procedure with arguments :',args
+                  print flightUtil.executeProcedureAndReturn(\
+                     'maybeInsertFlightDetails',\
+                     args)
+                  isStoredSuccessfuly = True
+                  if flightPrices=='':
+                     print 'Entered the Mysterious Flights'
+                  if isTracingEnabled:
+                    print 'Executed Procedure Successfuly'
+            except Exception, e:
+               if isStoredSuccessfuly:
+                  print 'Error parsing Flight Id/Fare/Timings'+str(e)
+               else:
+                  print 'Error Procedure Execution'+str(e)
+
+      except Exception, e:
+         print 'Unexpected Error '+str(e)
+         return False
+   gc.collect()
+   return True
+
+def retryFlight(retryNow):
+   retry = retryNow.getRoute()
+   parsed_json = fetchParsedJson(\
+                    route.getSource(),\
+                    route.getDestination(),\
+                    retryNow.getDate())
+   if parsed_json=='':
+      logError(\
+         'RetryError',\
+         retryNow.retryInformation(),\
+         5)
+      return
+   try:
+      flightPrefix = parsed_json['requestParams']['origin']+\
+                     parsed_json['requestParams']['destination']
+      flightSuffix = \
+         ''.join(reversed(\
+                 parsed_json['requestParams']['flight_depart_date'].split('/')))
+      cityNames = parsed_json['cityName']
+      flightSchedules = parsed_json['resultData']
+      #print 'Flight Schedules Parsed'
+      #print 'Total Flight Schedules: %s'%str(len(flightSchedules))
+   except Exception,e:
+      logError(\
+         'RetryError '+str(e),\
+         retryNow.retryInformation(),\
+         5)
+      return
+   pattern = flightUtil.flightSearchPattern(\
+                                            route.getSource(),\
+                                            route.getDestination(),\
+                                            date)
+   pattern = pattern.replace('%','')
+   if parseFlightScheduleAndStoreDetails(flightSchedules,pattern,route,date):
+      logError(\
+         'RetryError ',\
+         retryNow.retryInformation(),\
+         5)
+
+def maybeInsertFlightHistoryForDate(date):
+   retryQueue = Queue.Queue()
+   signal.signal(signal.SIGINT,flightUtil.handler)
+   gc.set_debug(gc.DEBUG_LEAK)
+   print date
+   cityPairs = cityPairList()
+   for pair in cityPairs:
+      route = flightUtil.Route(pair[0],pair[1])
+      print 'New Request'
+      print date,route.getSource(),route.getDestination()
+      parsed_json = fetchParsedJson(\
+                       route.getSource(),\
+                       route.getDestination(),\
+                       date)
+      if parsed_json=='':
+         #retryQueue.put(\
+         #   flightUtil.RetryFlight(\
+         #                         route,\
+         #                         date))
+         continue
+      if isTracingEnabled:
+         print 'Flight Schedules Parsed'
+      try:
+         flightPrefix = parsed_json['requestParams']['origin']+\
+                        parsed_json['requestParams']['destination']
+
+         flightSuffix = \
+            ''.join(reversed(\
+                    parsed_json['requestParams']['flight_depart_date'].split('/')))
+         cityNames = parsed_json['cityName']
+         flightSchedules = parsed_json['resultData']
+         if isTracingEnabled:
+             print 'Result Data Parsed'
+             print 'Total Flight Schedules: %s'%str(len(flightSchedules))
+      except Exception,e:
+         print 'Error :: Insufficient Data in Parsed Json'+str(e)
+         #$ print parsed_json
+         #logError(str(e),'',4)
+         #retryQueue.put(\
+         #   flightUtil.RetryFlight(\
+         #                         route,\
+         #                         date))
+         continue
+      pattern = flightUtil.flightSearchPattern(\
+                                               route.getSource(),\
+                                               route.getDestination(),\
+                                               date)
+      pattern = pattern.replace('%','')
+      parseFlightScheduleAndStoreDetails(flightSchedules,pattern,route,date)
+      #$ retryQueue.put(\
+      #$    flightUtil.RetryFlight(\
+      #$                          route,\
+      #$                          date))
+   #while not retryQueue.empty():
+   #    retryFlight(retryQueue.get())
+   sys.exit(0) 
+
+allMonths = [32,29,32,31,32,31,32,32,31,32,31,32]
+
+year = '2015'
+currMonth = 10
+endMonth = 10
+if __name__=="__main__":
+   year = sys.argv[1]
+   currMonth = int(sys.argv[2])
+   endMonth = int(sys.argv[3])
+   isResetNeeded = ''
+   if len(sys.argv)==5:
+      isResetNeeded = sys.argv[4]
+   sys.stdout = \
+      open('/home/abhinav/c++/python/logDev'+year+str(currMonth)+'.out','w+')
+   sys.stderr = \
+      open('/home/abhinav/c++/python/logDev'+year+str(currMonth)+'.err','w+')
+   if isResetNeeded!='':
+      resetTheAvailability()
+      print "Reset Availability"
+   q = Queue.Queue()
+   gc.set_debug(gc.DEBUG_LEAK)
+   def formatInTwoDigit(num):
+      if num/10==0:
+         num = '0'+str(num)
+      return str(num)
+   months = allMonths[currMonth-1:endMonth]
+   dates = \
+      [(j,i+1) for i in range(currMonth-1,endMonth) for j in range(1,allMonths[i])]
+   for date in dates:
+      iterator = 0
+      while iterator < len(date):
+         if q.qsize() < 2:
+            d = formatInTwoDigit(date[0])+'/'+formatInTwoDigit(date[1])+'/'+year
+            pid = multiprocessing.Process(target=maybeInsertFlightHistoryForDate,args=(d,),name=(str(date)))
+            q.put(pid)
+            pid.start()
+            iterator = iterator+1
+         else:
+            prevSize = q.qsize()
+            for i in range(prevSize):
+                pid = q.get()
+                if pid.is_alive():
+                   q.put(pid)
+            time.sleep(5)
+   sys.stdout.close()
+   sys.stderr.close()
+
+print 'exiting'
+sys.exit(0)
---
+++ flightUtil.py
@@ -0,0 +1,412 @@
+#!/usr/bin/python
+import MySQLdb
+import re,sys,gc,json,datetime,signal,traceback,random
+import urllib2,gzip
+import cStringIO
+
+appError = 0
+sysError = 0
+
+airlineNameQuery = "SELECT name FROM airline where code='%s'"
+airlineQuery = 'SELECT name, code FROM airline'
+cityNameQuery = "SELECT name FROM cityWithCode where code='%s'"
+cityQuery = "SELECT name,code FROM cityWithCode"
+directFlightsQuery =  "SELECT DISTINCT name,timings "\
+                      "FROM flights as A,directFlightsDetails as B "\
+                      "WHERE A.id=B.fid AND A.name LIKE '%s' "\
+                      "AND departureCity='%s' AND arrivalCity='%s'"\
+                      "AND A.isAvailable=1 AND B.isAvailable=1"
+oneStopFlightsQuery =  "SELECT DISTINCT name,timings "\
+                      "FROM flights as A,oneStopFlightsDetails as B "\
+                      "WHERE A.id=B.fid AND A.name LIKE '%s' "\
+                      "AND departureCity='%s' AND arrivalCity='%s'"\
+                      "AND A.isAvailable=1 AND B.isAvailable=1"
+twoStopFlightsQuery = "SELECT DISTINCT name,timings "\
+                      "FROM flights as A,twoStopFlightsDetails as B "\
+                      "WHERE A.id=B.fid AND A.name LIKE '%s' "\
+                      "AND departureCity='%s' AND arrivalCity='%s'"\
+                      "AND A.isAvailable=1 AND B.isAvailable=1"
+errorLoggigQuery =    "Insert into errorLog (traceBack,information,type) "\
+                      "values('%s','%s',%s)"
+priceHistoryQuery =   "SELECT price,DATE_FORMAT(price.sampleTime,'%%Y/%%m/%%d') as date "\
+                      "FROM flights as A,directFlightsDetails as B, directFlightsPrices as C"\
+                      "WHERE A.id=B.fid and B.id=C.sid and A.isAvailable=1 and B.isAvailable=1"\
+                      "and name='%s' ORDER BY date asc "\
+                      "UNION "\
+                      "SELECT price,DATE_FORMAT(price.sampleTime,'%%Y/%%m/%%d') as date "\
+                      "FROM flights as A,oneStopFlightsDetails as B, oneStopFlightsPrices as C"\
+                      "WHERE A.id=B.fid and B.id=C.sid and A.isAvailable=1 and B.isAvailable=1"\
+                      "and name='%s' ORDER BY date asc "\
+                      "UNION "\
+                      "SELECT price,DATE_FORMAT(price.sampleTime,'%%Y/%%m/%%d') as date "\
+                      "FROM flights as A,twoStopFlightsDetails as B, twoStopFlightsPrices as C"\
+                      "WHERE A.id=B.fid and B.id=C.sid and A.isAvailable=1 and B.isAvailable=1"\
+                      "and name='%s' ORDER BY date asc "
+metricPriceHistoryQuery = \
+                      "SELECT Date_FORMAT(sampleDate,'%%d/%%m/%%Y'), %s(price) "\
+                      "FROM directFlights "\
+                      "WHERE depart>='%s' and depart<='%s' "\
+                      "and departureCity='%s' and arrivalCity='%s' " \
+                      "and name LIKE '%s' "\
+                      "GROUP BY sampleDate"
+resetFlightAvailabilityQuery = \
+                      "UPDATE flights SET isAvailable=0"
+resetDirectFlightsDetailsAvailabilityQuery = \
+                      "UPDATE directFlightsDetails SET isAvailable=0"
+resetOneStopFlightsDetailsAvailabilityQuery = \
+                      "UPDATE oneStopFlightsDetails SET isAvailable=0"
+resetTwoStopFlightsDetailsAvailabilityQuery = \
+                      "UPDATE twoStopFlightsDetails SET isAvailable=0"
+
+errorLoggingQuery =   "INSERT INTO errorLog (traceBack,information,type)"\
+                      " VALUES('%s','%s','%s')"
+commit =              "commit"
+# SQL queries used in the system
+
+
+class Button:
+   def __init__(self,label,value):
+      self.label = label
+      self.value = value 
+   def setLabel(self,newLabel):
+      self.label = newLabel
+   def setValue(self,newValue):
+      self.value = newValue
+   def getLabel(self):
+      return self.label
+   def getValue(self):
+      return self.value
+
+
+class Route:
+   def __init__(self,source,destination):
+      self.source = source
+      self.destination = destination
+   def setSource(self,newSource):
+      self.source = newSource
+   def setDestination(self,newDestination):
+      self.destination = newDestination
+   def getSource(self):
+      return self.source
+   def getDestination(self):
+      return self.destination
+   def printRoute(self):
+      print self.source,self.destination
+   def routeInformation():
+      return self.source+self.destination
+
+class RetryFlight:
+   def __init__(self,route,date):
+      self.route = route
+      self.date = date   
+   def getRoute():
+      return self.route
+   def getDate():
+      return self.date
+   def retryInformation():
+      return self.route.routeInformation()+self.date 
+
+class Flight:
+   def __init__(self,route,date,price,airline):
+      self.route = route
+      self.date = date
+      self.price = price
+      self.airline = airline
+class Journey:
+   def __init__(self,departure,arrival):
+      self.departure = departure
+      self.arrival = arrival
+   def getDeparture():
+      return self.departure
+   def getArrival():
+      return self.arrival
+
+
+class DirectFlight(Flight):
+   def __init__(self,route,date,price,airline,journey):
+      Flight.__init__(self,route,date,price,airline)
+      self.journey = journey
+
+class OneStopFlight(Flight):
+   def __init__(self,route,date,price,airline,firstJourney,lastJourney):
+      Flight.__init__(self,route,date,price,airline)
+      self.firstJourney = firstJourney
+      self.lastJourney = lastJourney
+
+class TwoStopFlight(Flight):
+   def __init__(self,route,date,price,airline,firstJourney,middleJourney,lastJourney):
+      Flight.__init__(self,route,date,price,airline)
+      self.firstJourney = firstJourney
+      self.middleJourney = middleJourney
+      self.lastJourney = lastJourney
+
+months = {'01':'January',\
+          '02':'February',\
+          '03':'March',\
+          '04':'April',\
+          '05':'May',\
+          '06':'June',\
+          '07':'July',\
+          '08':'August',\
+          '09':'September',\
+          '10':'October',\
+          '11':'November',\
+          '12':'December',\
+          '':'Select'}
+def diff(a,b):
+   # return the difference of two lists
+   b = set(b)
+   return [aa for aa in a if aa not in b]
+
+def handler(signum,frame):
+   print traceback.print_stack()
+   sys.exit(0)
+
+
+def currentDate():
+   #return the current date in system specific way %Y %M %D
+   i = datetime.datetime.now()
+   return '%s %s %s'%(i.year,i.month,i.day) 
+
+
+def slotList():
+   # return the list of Slots. Currently Slots are of 1 hour
+   # and multiple of integer
+   type1Slot='0%s:00'
+   # pattern for slots from 00:00 to 09:00
+
+   type2Slot='%s:00'
+   # pattern for slots from 10:00 to 23:00
+
+   slotList=[]
+   for i in range(10):
+     slotList.append(type1Slot%str(i))
+   for i in range(11,24):
+     slotList.append(type2Slot%str(i))
+   return slotList
+
+def flightPrefix(orig,dest):
+   # returns the flight prefix that is orig+dest
+   # for e.g 'DEL'+'BLR' = 'DELBLR'
+   return orig+dest
+
+def flightSufix(yyyymmdd):
+
+  yyyymmdd = yyyymmdd.replace('/',' ')
+  yyyymmdd = yyyymmdd.replace('-',' ')
+  yyyymmdd = yyyymmdd.split()
+  yyyymmdd = yyyymmdd[2]+yyyymmdd[1]+yyyymmdd[0]
+  return yyyymmdd
+
+def jsonList(lst):
+   jsn = []
+   for l in lst:
+      d = {}
+      d[str(l[1])]=str(l[0])
+      jsn.append(d)
+   return json.dumps(jsn)
+
+def flightQuerySufix(yyyymmdd):
+  # creates suffix of search pattern from date
+  # %Y%M%D 
+  yyyymmdd = yyyymmdd.replace('/',' ')
+  yyyymmdd = yyyymmdd.replace('-',' ')
+  yyyymmdd = yyyymmdd.split()
+  yyyymmdd = yyyymmdd[2]+yyyymmdd[0]+yyyymmdd[1]
+  return yyyymmdd
+
+def flightSearchPattern(orig,dest,date):
+   # returns search pattern for flight.
+   # 'PREFIX%SUFIX'
+   prefix = flightPrefix(orig,dest)
+   sufix = flightSufix(date)
+   return prefix+'%'+sufix
+
+def flightQueryPattern(airline,date):
+   prefix = airline
+   sufix = flightQuerySufix(date)
+   return [prefix,'%'+sufix+'%']
+
+def flightQueryPattern(airline,orig,date):
+
+   airlinePattern = ''
+   if airline=='':
+      airlinePattern = '__'
+   else:
+      airlinePattern = airline
+   suffix = flightQuerySufix(date)
+
+   #if flightType=='direct':
+   #   return orig+dest+'%'+airlinePattern+'%'+suffix 
+   #elif flightType=='oneStop' or flightType=='twoStop':
+   #   return orig+'%'+airlinePattern+'%'+suffix
+   #return ''
+   return orig+'%'+airlinePattern+'%'+suffix
+
+def findAndSanitizeInput(response):
+   match = ''
+   try:
+      for line in response:
+         data = re.search("eagerFetch",line)
+         if data:
+            match = line
+            break;
+      splited = match.replace(';','=').split('=',9)
+      return False,splited[1]
+   except Exception,e:
+      return True,splited
+   
+def createConnection():
+   try:
+      cnx =  MySQLdb.connect(\
+                          host='127.0.0.1',\
+                          user='root',\
+                          passwd='root',\
+                          db='flightDetails')
+                          #db='flightPricesExperiment')
+                          #db='flightPricesBackup')
+                          #db='flightPrices')
+   except Exception, e:
+      print str(e)
+      sys.exit(1)
+   return cnx
+
+#def connection(cnx=createConnection()):
+def connection():
+   cnx = createConnection()
+   return cnx
+
+def executeQueryAndReturn(query):
+   try:
+      cnx = connection()
+      cur = cnx.cursor()
+      cur.execute(query)
+      print query
+      data = cur.fetchall()
+      cnx.close()
+   except Exception,e:
+      return 'Error'+str(e)
+   return data
+
+def executeProcedureAndReturn(procName,args):
+   cnx = connection()
+   cur = cnx.cursor()
+   cur.callproc(procName,args)
+   result = cur.fetchall()
+   cnx.close()
+   return result
+
+def cityPairList():
+   cnx = connection()
+   cur = cnx.cursor()
+   cur.execute(cityQuery)
+   dataDuplicate = cur.fetchall()
+   data = set(dataDuplicate)
+   cnx.close()
+   return [(city1[0],city2[0]) for city1 in data for city2 in data if city1[0]!=city2[0]]
+
+def fetchCityPairs(cityPair=cityPairList()):
+   return cityPair
+
+def airlineName(code):
+   airlines = executeQueryAndReturn(airlineQuery)
+   for airline in airlines:
+      if code==airline[1]:
+         return airline[0]
+   return code
+ 
+def flightDescriptionPair2(cityList,timings): 
+   fstr=''
+   iterLen = len(cityList)-1
+   for i in range(iterLen):
+      fstr += cityList[i]+\
+              '( '+timings[2*i]+' )'+\
+              ' >->> '+\
+              '( '+timings[2*i+1]+' )'
+   fstr += cityList[iterLen]
+   return fstr
+ 
+def flightDescription(code,timings):
+   city1=''
+   city2=''
+   city3=''
+   city4=''
+   prefix = airlineName(code[6:8])
+   pair1 = code
+   pair2 = prefix+ ' '
+   timings = timings.split(' ')
+   sufix = [timing+' ' for timing in timings]
+   cities = executeQueryAndReturn(cityQuery)
+   def cityNames(codes):
+      names = []
+      for code in codes:
+         name = code
+         for city in cities:
+            if city[1]==code:
+                name = city[0]
+                break
+         names.append(name)
+      return names
+   city1 = code[0:3]
+   city2 = code[3:6]
+   city3Pos = code.find(city2,7)
+   if city3Pos==-1:
+      pair2 += flightDescriptionPair2(\
+                 cityNames([city1,city2]),\
+                 sufix)
+      return (pair1,pair2) 
+   city3 =code[city3Pos+3:city3Pos+6]
+   city4Pos = code.find(city3,city3Pos+7)
+   if city4Pos==-1:
+      pair2 += flightDescriptionPair2(\
+                 cityNames([city1,city2,city3]),\
+                 sufix)
+      return (pair1,pair2) 
+   city4 = code[city4Pos+3:city4Pos+6]
+   pair2 += flightDescriptionPair2(\
+              cityNames([city1,city2,city3,city4]),\
+              sufix)
+   return (pair1,pair2) 
+
+
+#date = 29/11/15
+def fetchSanitizedInput(orig,dest,date,travlr=1):
+   uniq = random.randint(100000000,999999999)
+   url = 'http://www.yatra.com/air-search/dom2/trigger?'\
+         'type=O&'\
+         'viewName=normal&'\
+         'flexi=0&'\
+         'noOfSegments=1&'\
+         'origin=%s&'\
+         'originCountry=IN&'\
+         'destination=%s&'\
+         'destinationCountry=IN&'\
+         'flight_depart_date=%s&'\
+         'ADT=%s&CHD=0&INF=0&class=Economy&'\
+         'hb=False&unique=%d'%(orig,dest,date,travlr,uniq)
+   request = urllib2.Request(url)
+   request.add_header('Host','www.yatra.com')
+   request.add_header('Accept','*/*')
+   request.add_header('Accept-Language','en-US,en;q=0.5')
+   request.add_header('Accept-Encoding','gzip, deflate')
+   request.add_header('X-Requested-With','XMLHttpRequest')
+   #request.add_header('Connection','keep-alive')
+   request.add_header('User-Agent',"Mozilla/5.0 (Linux x86_64) Gecko/20100101 Firefox/41.0") #$
+   #@ print 'Request :',request.headers.items()
+   response = urllib2.urlopen(request)
+   #@ print 'Response :',response.info()
+   
+   if response.info().get('Content-Encoding').find('gzip')!=-1:
+       f = gzip.GzipFile(fileobj = cStringIO.StringIO(response.read()))
+       line = f.readline()
+       while line!='':
+          try:
+             if re.search("eagerFetch",line):
+                line = line.replace(';','=').split('=',4)
+                return False,line[1]
+          except Exception,e:
+                return True,line
+          line = f.readline()
+   return False,line 
+
+
