f1e73eaa75c8bdedc67bcdee4fdd37e7372a62c8
Abhinav <abhinav@www.squirrelmail.com>
2016-01-09 11:58:58 +0530

    procedure-2.0

--- procedure.sql
+++ procedure.sql
@@ -1,67 +1,71 @@
 begin
-      declare flightId int(11);
-      declare pid int;
-      declare cnt int;
-      declare arrive char(5);
-      declare departure char(5);
-      declare stopId int;
-      declare stopId1 int;
-      declare stopId2 int;
-      declare prc int(11) unsigned;
-      declare pass int;
-      
-      declare exit handler for sqlexception 
-      begin
-         rollback;
-      end;
-      start transaction;
-      set prc = 0;
-                
-      update flights set isAvailable=1 where name=flightName;
-      select id, count(*) into flightId, cnt from flights where name=flightName;
-      if cnt=0 then
-         insert into flights(name,departureCity,arrivalCity,isAvailable) values (flightName,departureCity,arrivalCity,1);
-         select last_insert_id() into flightId;
-      end if;
+declare timingsLength smallint;
+declare cnt smallint;
+declare flightId int(11);
+declare stopId int;
+declare pid int;
+declare prc int(11) unsigned;
 
-      select substr(timings,1,5) into departure;
-      select substr(timings,7) into timings;
-      select substr(timings,1,5) into arrive;
-      select substr(timings,7) into timings;
+declare exit handler for sqlexception 
+begin
+   rollback;
+end;
 
-      select id into stopId from stop where departure=depart and arrival=arrive and fid=flightId;
-      if not exists (select id from stop where departure=depart and arrival=arrive and fid=flightId) then
-        insert into stop(depart,arrival,fid) values(departure,arrive,flightId);
-        select last_insert_id() into stopId;
-      end if;
+start transaction;
+set prc = 0;
+
+select id, count(*) into flightId, cnt from flights where name=flightName;
+
+if cnt=0 then
+   insert into flights(name,departureCity,arrivalCity,isAvailable)    
+   values (flightName,departureCity,arrivalCity,1);
+   select last_insert_id() into flightId;
+else
+   update flights set isAvailable=1 where name=flightName;
+end if;
+
+select length(times) into timingsLength;
+if timingsLength <= 12 then
+   select id into stopId from directFlightsDetails where fid=flightId and timings=times;
+   if not exists (select id from directFlightsDetails where fid=flightId and timings=times) then
+      insert into directFlightsDetails(timings,fid,isAvailable) values(times,flightId,1);
+      select last_insert_id() into stopId;
+   else 
+        update directFlightsDetails set isAvailable=1 where fid=flightId and timings=times;
+   end if;
      
-      select id, price into pid, prc from price where sid=stopId limit 1;
-      if not exists (select id from price where sid=stopId) or priceOfFlight!=prc then
-        insert into price (price,sid) values(priceOfFlight,stopId);
-      end if;
-      
-      if timings!=' ' then
-         select substr(timings,1,5) into departure;
-         select substr(timings,7) into timings;
-         select substr(timings,1,5) into arrive;
-         select substr(timings,7) into timings;
+   select id, price into pid, prc from directFlightsPrices where sid=stopId order by id desc limit 1;
+   if not exists (select id from directFlightsPrices where sid=stopId) or priceOfFlight!=prc then
+      insert into directFlightsPrices (price,sid) values(priceOfFlight,stopId);
+   end if;
 
-         select id into stopId1 from stop1 where depart=departure and arrival=arrive and sid=stopId;
-         if not exists (select id from stop1 where depart=departure and arrival=arrive and sid=stopId) then
-           insert into stop1(depart,arrival,sid) values(departure,arrive,stopId);
-           select last_insert_id() into stopId1;
-         end if;
+elseif timingsLength <= 25 then
+   select id into stopId from oneStopFlightsDetails where fid=flightId and timings=times;
+   if not exists (select id from oneStopFlightsDetails where fid=flightId and timings=times) then
+      insert into oneStopFlightsDetails(timings,fid,isAvailable) values(times,flightId,1);
+      select last_insert_id() into stopId;
+   else 
+        update oneStopFlightsDetails set isAvailable=1 where fid=flightId and timings=times;
+   end if;
+     
+   select id, price into pid, prc from oneStopFlightsPrices where sid=stopId order by id desc limit 1;
+   if not exists (select id from oneStopFlightsPrices where sid=stopId) or priceOfFlight!=prc then
+      insert into oneStopFlightsPrices (price,sid) values(priceOfFlight,stopId);
+   end if;
 
-         if timings!=' ' then
-            select substr(timings,1,5) into departure;
-            select substr(timings,7) into timings;
-            select substr(timings,1,5) into arrive;
-            select substr(timings,7) into timings;
-            select id into stopId2 from stop2 where depart=departure and arrival=arrive and sid=stopId1;
-            if not exists (select id from stop2 where depart=departure and arrival=arrive and sid=stopId1) then
-               insert into stop2(depart,arrival,sid) values(departure,arrive,stopId1);
-            end if;
-         end if;
-      end if;
-      commit;
+elseif timingsLength <= 36 then
+   select id into stopId from twoStopFlightsDetails where fid=flightId and timings=times;
+   if not exists (select id from twoStopFlightsDetails where fid=flightId and timings=times) then
+      insert into twoStopFlightsDetails(timings,fid,isAvailable) values(times,flightId,1);
+      select last_insert_id() into stopId;
+   else 
+        update twoStopFlightsDetails set isAvailable=1 where fid=flightId and timings=times;
+   end if;
+     
+   select id, price into pid, prc from twoStopFlightsPrices where sid=stopId order by id desc limit 1;
+   if not exists (select id from twoStopFlightsPrices where sid=stopId) or priceOfFlight!=prc then
+      insert into twoStopFlightsPrices (price,sid) values(priceOfFlight,stopId);
+   end if;
+end if;
+commit;
 end
